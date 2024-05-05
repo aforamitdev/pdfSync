7442d1a02df6cdd92e81ab05d45164120873a45b
aforamitdev <aforamitrai@gmail.com>
2024-05-05 20:46:07 +0530

    added initial configurations

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button, Flex, Text } from '@radix-ui/themes';
+
+function App() {
+  return (
+    <>
+      <Flex direction='column' gap='2'>
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
+    </>
+  );
+}
+
+export default App;
